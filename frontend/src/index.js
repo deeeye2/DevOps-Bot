084a6630439b6fda774efd678ea5090b7f9007ef
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-06-06 12:48:47 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
-
-
+// index.js
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
